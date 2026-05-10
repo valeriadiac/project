ab22ab5710057c45360daa3a829825e43cf017c9
Myrto Anastasiadi <myrtoanastasiadii@gmail.com>
2026-05-10 21:09:49 +0300

    Doctor Updated

--- Doctor.java
+++ Doctor.java
@@ -2,18 +2,54 @@ public class Doctor extends Person {
 
     private static int nextID= 0;
     private int id;
-    private String doctorName;
-    private int doctorPhone;
     private String specialty;
     private int years;
 
-    public Doctor(int id, String doctorName, int doctorPhone, String specialty, int years) {
+    public Doctor(String name, int phone, String specialty, int years) {
 
-        super(id, doctorName, doctorPhone);
+        super(name, phone);
+        this.id = nextID++;
         this.specialty = specialty;
         this.years = years;
 
     }
 
+    public Doctor(int id, String name, int phone, String specialty, int years) {
 
+        super(name, phone);
+        this.id = id;
+        if (id >= nextID) nextID = id +1;
+        this.specialty = specialty;
+        this.years = years;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    public int getYears() {
+        return years;
+    }
+
+    public void setSpecialty(String specialty) {
+        this.specialty = specialty;
+    }
+
+    public void setYears(int years) {
+        this.years = years;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Doctor ID: %d | Name: %s | Phone: %d | Specialty: %s | Years of Experience: %d",
+                id, getName(), getPhone(), specialty, years);
+    }
+
+    public String toFileString() {
+        return id + "," + getName() + "," + getPhone() + "," + specialty + "," + years;
+    }
 }
