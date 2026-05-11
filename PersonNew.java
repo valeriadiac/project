ec07136693e4a9af5762642ea23c0d3d0b459c5c
Myrto Anastasiadi <myrtoanastasiadii@gmail.com>
2026-05-11 21:16:27 +0300

    Person different

---
+++ PersonNew.java
@@ -0,0 +1,31 @@
+public class Person {
+
+    private String name;
+    private int phone;
+
+    public Person(String name, int phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Name: %s | Phone: %d", name, phone);
+    }
+}
