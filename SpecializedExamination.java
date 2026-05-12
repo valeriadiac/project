82d8dbd7950b10b9c3a90701dacaaa1c38d7abef
Myrto Anastasiadi <myrtoanastasiadii@gmail.com>
2026-05-12 18:14:47 +0300

    Add SpecializedExamination class for specialized exams

---
+++ SpecializedExamination.java
@@ -0,0 +1,42 @@
+public class SpecializedExamination extends Exam {
+
+    private String specialty;
+    private static final double costIncreaseRate = 0.30;
+
+    public SpecializedExamination(String examName, int maxSlots, double cost, int doctorId, String specialty) {
+        super(examName, "Specialized", maxSlots, cost, doctorId);
+        this.specialty = specialty;
+    }
+
+    public SpecializedExamination(int id, String examName, int maxSlots, double cost, int doctorId, String specialty) {
+        super(id, examName, "Specialized", maxSlots, cost, doctorId);
+        this.specialty = specialty;
+    }
+
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(String specialty) {
+        this.specialty = specialty;
+    }
+
+    public double getCost(boolean fastResults){
+        if (fastResults) {
+            return getExamCost() + (getExamCost() *costIncreaseRate);
+        } else {
+            return getExamCost();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + String.format(" | Specialty: %s", specialty);
+    }
+
+    @Override
+    public String toStringFile() {
+        return super.toStringFile() + "," + specialty;
+    }
+    
+}
