34850767a3a6e2a06e52b94f2822ce9f2df2b37e
valeriadiaconu <valeriadiaconu04@gmail.com>
2026-06-18 18:13:10 +0300

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,12 +1,4 @@
-/* 
-MYRTO ANASTASIADI ALEXIOU
-AM: 3250008
-Webmail: p3250008@aueb.gr
---------------------------
-VALERIA DIACONU
-AM: 3250238
-Webmail: p3250238@aueb.gr 
-*/
+
 
 public class Person {
     private String name;
